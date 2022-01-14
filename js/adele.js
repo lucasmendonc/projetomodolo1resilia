ca7d0962153eb7f6386ecf3e08437b69d27f7328
Lucas Mendonça <lucas.mendoncaaa@gmail.com>
2022-01-13 21:11:28 -0300

    pronto

--- js/adele.js
+++ js/adele.js
@@ -4,7 +4,7 @@ var pergunta = prompt("1- Lançar um agudo dizendo 'o estado é laicoo' \n2- Diz
 while (pergunta != 1 || pergunta != 2) {
 
 if (pergunta == 1) {
-    window.location.href = "../adele/adele2.html";
+    window.location.href = "../adele/adele1.html";
 /*para finalizar e direcionar para o html acima caso escolha 1 ---> */ 
    break;
   } else if (pergunta == 2) {
